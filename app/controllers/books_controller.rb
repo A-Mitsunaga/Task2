99c830509147bbd11609618abcac3c09291bd99f
A-mitsunaga <a56st70410@icloud.com>
2021-05-01 05:28:48 +0000

    [Finish]task

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,24 +2,24 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
-   book = Book.new(book_params)
-   book.save
-   redirect_to book_path(id: book)
-   if book.save
-  flash[:notice] = "Book was successfully created."
-   end
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully saved."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render action: :index
+    end
   end
 
   def show
     @book = Book.find(params[:id])
   end
 
-  def new
-   @book = Book.new
-  end
 
   def edit
     @book = Book.find(params[:id])
@@ -27,9 +27,14 @@ class BooksController < ApplicationController
 
   def update
    book = Book.find(params[:id])
-   book.update(book_params)
-   redirect_to book_path(book)
-   flash[:notice] = "Book was successfully updated."
+
+   if book.update(book_params)
+     flash[:notice] = "Book was successfully updated."
+     redirect_to book_path(book.id)
+   else
+     flash[:notice] = "error"
+     render action: :edit
+   end
   end
 
   def destroy
